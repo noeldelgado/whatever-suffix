2066ab6653a57b59729f295d83b2e543422abc82
Noel Delgado <auhcsei@gmail.com>
2020-04-04 12:28:47 -0600

    :recycle: new action fonts.loadFont

--- src/actions/fonts.js
+++ src/actions/fonts.js
@@ -3,26 +3,39 @@ import WebFont from 'webfontloader';
 import { rand } from '/utils';
 
 /**
- * Selects a random font from the `fonts` state registry and loads it
+ * Loads a font from Google Fonts
  * @async
- * @return <Promise>
+ * @param {string} family - font family name
+ * @return {Promise<string|Error>}
  */
-export const loadRandomFont = async (store) => {
-  count('loadRandomFont')
-  const { fonts, fontsLen } = store.state;
-  const font = rand(fonts, fontsLen)?.family;
-
+export const loadFont = async (store, family) => {
+  if (!family) throw new Error('No FontFamilyName given')
   return new Promise((resolve, reject) => {
     WebFont.load({
       classes: false,
-      google: { families: [font] },
-      active: () => {
-        store.setState({ font });
-        resolve();
-      },
-      fontinactive: (familyName) => {
-        reject(new Error(`Font “${familyName}” cannot be loaded`));
-      }
+      google: { families: [family] },
+      active: () => resolve(family),
+      fontinactive: (familyName) => reject(new Error(`Font “${familyName}” cannot be loaded`))
     });
   });
+}
+
+/**
+ * Selects a random font from the `fonts` state registry, loads it and sets
+ * state.font if resolved
+ * @async
+ * @return {Promise<undefined|Error>}
+ */
+export const loadRandomFont = async (store) => {
+  count('loadRandomFont');
+  const { fonts, fontsLen } = store.state;
+  const font = rand(fonts, fontsLen)?.family;
+
+  try {
+    await store.actions.fonts.loadFont(font);
+    store.setState({ font });
+  }
+  catch (err) {
+    throw err;
+  }
 };

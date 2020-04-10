0ac09bb4dbc6a7e3ce68bc8fcffe6bd4c28e3daa
Noel Delgado <auhcsei@gmail.com>
2020-04-09 21:02:07 -0600

    :sparkles: outlined logo shapes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,23 +1,37 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import { Box } from '@material-ui/core';
 import useGlobal from '/store';
 import styles from './Logo.module.css';
+
+const internals = {
+  reChar: new RegExp(/[a-z]|[A-Z]/),
+  textColorFilled: 'var(--app-color-main)',
+  textColorOutlined: 'var(--app-color-text)'
+};
 
 const Logo = () => {
   const [logoShape] = useGlobal(state => state.logoShape);
   const [mainWord] = useGlobal(state => state.mainWord);
   const [suffix] = useGlobal(state => state.suffix);
+  const [textColor, setTextColor] = useState('');
 
   const wordFirstLetter = mainWord.charAt(0).toUpperCase();
   const wordSecondLetter = mainWord.charAt(1).toUpperCase();
-  const suffixFirstLetter = suffix?.match(/[a-z]|[A-Z]/)?.[0].toUpperCase() ?? wordSecondLetter;
+  const suffixFirstLetter = suffix?.match(internals.reChar)?.[0].toUpperCase() ?? wordSecondLetter;
+
+  useEffect(() => {
+    if (logoShape.type === 'fill')
+      setTextColor(internals.textColorFilled);
+    else
+      setTextColor(internals.textColorOutlined);
+  }, [logoShape]);
 
   return (
     <Box position='relative' mb={2}>
       <svg className={styles.svg} viewBox="0 0 100 100">
         {logoShape.path}
       </svg>
-      <Box className={styles.text}>
+      <Box className={styles.text} style={{color: textColor}}>
         {wordFirstLetter}{suffixFirstLetter}
       </Box>
     </Box>
